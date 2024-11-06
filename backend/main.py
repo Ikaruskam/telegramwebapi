966fbfb8d5323eea9329a0dd61c61e0384990211
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-06 11:57:10 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine, Column, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./items.db"  # Путь к вашей базе данных SQLite
+DATABASE_URL = "sqlite:///./test.db"  # Путь к вашей базе данных SQLite
 
 # Настройка SQLAlchemy
 Base = declarative_base()
