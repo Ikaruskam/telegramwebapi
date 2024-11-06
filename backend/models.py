ba0c621c8db520a73273b3eebf64b1a381d1d62b
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-06 12:11:30 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -5,6 +5,7 @@ class User(Base):
     __tablename__ = "user_data"
 
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(String, index=True)
     name = Column(String, index=True)
     weight = Column(Integer)
     height = Column(Integer)
