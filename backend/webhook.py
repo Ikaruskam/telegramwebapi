9c72b5cb4906b2d5e8ac9c87e7bb1da2f6f01d53
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-06 11:44:30 +0300

    Update webhook.py

--- backend/webhook.py
+++ backend/webhook.py
@@ -4,7 +4,7 @@ TOKEN = 'YOUR_BOT_TOKEN'  # Замените на ваш токен
 URL = f'https://api.telegram.org/bot{TOKEN}/setWebhook'
 
 def set_webhook():
-    webhook_url = 'https://yourdomain.com/webhook'  # Замените на URL вашего сервера
+    webhook_url = 'https://tvoitrenerbot.ru/webhook'  # Замените на URL вашего сервера
     response = requests.post(URL, json={'url': webhook_url})
     
     if response.status_code == 200:
