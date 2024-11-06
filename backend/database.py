178fdb1c60abcdbef22ff4fbb86ab7286b5e65aa
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-06 12:10:17 +0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import os
 
-# Используем SQLite в качестве базы данных
-DATABASE_URL = "sqlite:///./test.db"  # файл базы данных будет создан в корне проекта
+# Путь к файлу базы данных SQLite
+DATABASE_URL = "sqlite:///./backend/test.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
