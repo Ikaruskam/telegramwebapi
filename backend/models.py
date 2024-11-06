fd4ae1da87324fdf3cf0b104cd5f0b634ce2db22
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-06 12:11:00 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from database import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "user_data"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
