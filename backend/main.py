c0f8461bd25981edcbd3859eeb7adc6407325e67
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-07 14:31:17 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,47 +1,52 @@
-import random
-from fastapi import FastAPI
+import os
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from dotenv import load_dotenv
 import uvicorn
 
+# Загружаем переменные окружения
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///app/backend/test.db")
 
 app = FastAPI()
 
+# Инициализация базы данных SQLite с SQLAlchemy
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
 
+# Модель базы данных
+class Item(Base):
+    __tablename__ = "items"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+
+# Создаём таблицу, если её нет
+Base.metadata.create_all(bind=engine)
+
+# Получение сессии базы данных
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Эндпоинт для работы с элементами
 @app.get("/items")
-def get_items():
-    items = [
-        {
-            "id": 1,
-            "name": "Docker",
-            "img": "https://static-00.iconduck.com/assets.00/docker-icon-2048x2048-5mc7mvtn.png",
-        },
-        {
-            "id": 2,
-            "name": "Nginx",
-            "img": "https://www.svgrepo.com/show/373924/nginx.svg",
-        },
-        {
-            "id": 3,
-            "name": "GitHub",
-            "img": "https://cdn-icons-png.flaticon.com/512/25/25231.png",
-        },
-    ]
-    random.shuffle(items)
-    return items
-
+def read_items(db: Session = Depends(get_db)):
+    return db.query(Item).all()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        # "http://localhost:5173",
-        # "http://31.129.43.117",
-        "https://tvoitrenerbot.ru",
-    ],
+    allow_origins=["https://tvoitrenerbot.ru"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
