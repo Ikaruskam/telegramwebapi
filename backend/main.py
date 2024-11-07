b9696c485b3cd0761b17fb14660dfa898d323f94
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-07 15:04:03 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,7 @@
 import random
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel  # Импортируем BaseModel из pydantic для работы с моделями
 import uvicorn
 
 app = FastAPI()
