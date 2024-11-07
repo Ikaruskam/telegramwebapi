c82fb3c30b91be150d192cced4b34988c5ee28a7
Ikaruskam <45374569+Ikaruskam@users.noreply.github.com>
2024-11-07 14:53:48 +0300

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+from .database import Base
 
-class User(Base):
-    __tablename__ = "user_data"
+class UserMessage(Base):
+    __tablename__ = "user_messages"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True)
-    name = Column(String, index=True)
-    weight = Column(Integer)
-    height = Column(Integer)
+    user_id = Column(Integer, index=True)
+    username = Column(String, index=True)
+    text = Column(String)
